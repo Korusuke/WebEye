8ddbd316477a15d72c3e5ae17fc52318c299e53e
rusherrg <rushang101@gmail.com>
2019-05-25 20:48:59 +0530

    Add React(Frontend) demo app

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import './App.css';
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
