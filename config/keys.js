9d7ce8ceaae17580b472c94e1d300ccf8a9c50a2
rusherrg <rushang101@gmail.com>
2019-05-26 12:55:46 +0530

    Add eslint configuration and pipeline test

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: "mongodb://teamprobably:prollymL4B5@ds147436.mlab.com:47436/webeye" 
+  mongoURI: 'mongodb://teamprobably:prollymL4B5@ds147436.mlab.com:47436/webeye' 
 };
