4c00bbc5b0117af7705c923b048b382ba332b7bb
rusherrg <rushang101@gmail.com>
2019-05-26 12:55:46 +0530

    Add eslint configuration and pipeline test

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 // Import libraries
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 // Import Scripts for Routes
-const users = require("./server/signin/users");
+const users = require('./server/signin/users');
 
 const app = express();
 
 // Bodyparser middleware
 app.use(
-	bodyParser.urlencoded({
-		extended: false
-	})
+  bodyParser.urlencoded({
+    extended: false
+  })
 );
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-	.connect(db, { useNewUrlParser: true })
-	.then(() => console.log("MongoDB successfully connected"))
-	.catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB successfully connected'))
+  .catch(err => console.log(err));
 
 // Routes
-app.use("/", users);
-app.get("/", function(req, res){
-    res.send("Hey!");
+app.use('/', users);
+app.get('/', function(req, res){
+  res.send('Hey!');
 });
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
