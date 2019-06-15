67ebc8aeb7c8a84e15b49a2583b4097976da45e1
Aditya Prajapati <aditya10699@gmail.com>
2019-06-15 12:21:34 +0530

    Change redirect from permanent(301) to temp(307), and added code to maintain visit count

--- server/redirect.js
+++ server/redirect.js
@@ -13,41 +13,41 @@ const mongourl=`mongodb://${user}:${password}@ds261626.mlab.com:61626/webeye-adi
 mongoose.connect(mongourl, {useNewUrlParser: true});
 
 
-router.all('/*+', function(req, res) {
-  console.log('getting all objects');
+router.get('/*+', function(req, res) {
   var temp=req.originalUrl;
   const url=`http://localhost:7000${temp}`;
   console.log(url);
+  console.log()
   mapping.find({'murl':url}).exec(function(err,result){
     if(!err){
-      // res.redirect(301, result.ogurl);
-      // res.redirect(result[0].ogurl);
-      const red=result[0].ogurl;
-      res.status(301).redirect(red);
+      try{
+        const red=result[0].ogurl;
+        res.status(307).redirect(red);
+        var ip = req.connection.remoteAddress;
+        mapping
+        .findOneAndUpdate({"murl":url},{$inc:{visits:1}})
+        .exec(function(err,result){
+          if(!err){
+            console.log(result);  
+          }
+          else{
+            res.json({"msg":"Could not find minified url."});
+          }
+        });
+      }
+      catch(e){
+        res.json({
+          'msg':'URL does not exist',
+        });
+      }
     }
     else{
       res.json({
-        'msg': result,
+        'msg': err,
+        'str': 'Hey mate',
       });
     }
   });
 });
-
-// // used by the client when he opens the shortened url
-// router.post('',function(req,res){
-//   // ip : ip address of the device that opened the shortened url
-//   // url: the shortened url    
-//   console.log("Inside redirect.js");
-//   const ip=req.ip;
-//   const prot=req.protocol;
-//   const host=req.get('host');
-//   const url=`${prot}://${host}`;
-//   console.log(ip,url);
-//   redirect_url=getRedirectUrl(req.originalUrl);
-//   console.log(redirect_url);
-//   // Now that we have the shortened url and the ip address, we can use db to get the original url where the shortened url points
-//   // and also log the ip address and increase the counter by 1
-//   res.redirect(redirect_url);
-// });
 
 module.exports=router;
