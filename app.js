819dc3ef692a20ef2603b61f7a580a21e1379e8e
Aditya <aditya10699@gmail.com>
2019-08-28 18:51:06 +0530

    Added cookie parser

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
 var app = express();
-
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 // Use heroku env variable or port 7000
 const port = process.env.PORT || 7000;
 app.listen(port,function(){console.log('Hey, listening on port %s', port);});
